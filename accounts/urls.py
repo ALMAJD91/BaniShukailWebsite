d99edb0f9753fabe4c10e42c1c747517d5c19134
ALMAJD91 <149313563+ALMAJD91@users.noreply.github.com>
2025-12-19 23:35:24 +0400

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
     path("users/", views.users_view, name="users"),
 ]
