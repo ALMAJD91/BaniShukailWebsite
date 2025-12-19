0469cfdcc4bccae3f035483f4690faa5df5f1252
ALMAJD91 <149313563+ALMAJD91@users.noreply.github.com>
2025-12-19 21:38:36 +0400

    Create 0001_initial.py

---
+++ accounts/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+from django.db import migrations, models
+import django.utils.timezone
+import django.contrib.auth.validators
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = [
+        ("auth", "0012_alter_user_first_name_max_length"),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name="User",
+            fields=[
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("password", models.CharField(max_length=128, verbose_name="password")),
+                ("last_login", models.DateTimeField(blank=True, null=True, verbose_name="last login")),
+                ("is_superuser", models.BooleanField(default=False, verbose_name="superuser status")),
+                (
+                    "username",
+                    models.CharField(
+                        max_length=150,
+                        unique=True,
+                        validators=[django.contrib.auth.validators.UnicodeUsernameValidator()],
+                        verbose_name="username",
+                    ),
+                ),
+                ("first_name", models.CharField(blank=True, max_length=150, verbose_name="first name")),
+                ("last_name", models.CharField(blank=True, max_length=150, verbose_name="last name")),
+                ("email", models.EmailField(blank=True, max_length=254, verbose_name="email address")),
+                ("is_staff", models.BooleanField(default=False, verbose_name="staff status")),
+                ("is_active", models.BooleanField(default=True, verbose_name="active")),
+                ("date_joined", models.DateTimeField(default=django.utils.timezone.now, verbose_name="date joined")),
+                ("role", models.CharField(choices=[("ADMIN", "Admin"), ("EMPLOYEE", "Employee")], default="EMPLOYEE", max_length=20)),
+                ("groups", models.ManyToManyField(blank=True, to="auth.group")),
+                ("user_permissions", models.ManyToManyField(blank=True, to="auth.permission")),
+            ],
+            options={"abstract": False},
+        ),
+    ]
